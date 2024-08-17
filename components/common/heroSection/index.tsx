42a93b810e1b81853e77251b38beaf4ee6aefe1f
Mayank <mayankbudhiraja88@gmail.com>
2024-08-17 11:03:35 +0530

    Added Hero Section

--- components/common/heroSection/index.tsx
+++ components/common/heroSection/index.tsx
@@ -1,11 +1,37 @@
-import { PORTFOLIO_PERSON_NAME } from "@/constants/Portfolio";
+import {
+  HERO_CONTENT,
+  PERSON_ROLE,
+  PORTFOLIO_PERSON_NAME,
+} from "@/constants/Portfolio";
+import Image from "next/image";
 
 const HeroSection = () => {
   return (
     <div className="border-b border-neutral-900 pb-4 lg:mb-35">
-      Hero Section
-      <div>
-        <h1>{PORTFOLIO_PERSON_NAME}</h1>
+      <div className="flex flex-wrap">
+        <div className="w-full lg:w-1/2">
+          <div className="flex flex-col items-center lg:items-start">
+            <h1 className="pb-16 text-6xl font-thin tracking-tight lg:mt-16 lg:text-8xl">
+              {PORTFOLIO_PERSON_NAME}
+            </h1>
+            <span className="bg-gradient-to-r from-pink-300 via-slate-500 to-purple-500 bg-clip-text text-4xl tracking-tight text-transparent">
+              {PERSON_ROLE}
+            </span>
+            <p className="my-2 max-w-xl py-6 font-light tracking-tighter">
+              {HERO_CONTENT}
+            </p>
+          </div>
+        </div>
+        <div className=" w-full lg:w-1/2 lg:p-8">
+          <div className="flex justify-center">
+            <Image
+              src="/kevinRushProfile.png"
+              alt="profile-pic"
+              width={200}
+              height={200}
+            />
+          </div>
+        </div>
       </div>
     </div>
   );
--- constants/Portfolio/index.ts
+++ constants/Portfolio/index.ts
@@ -1,4 +1,5 @@
 export const PORTFOLIO_PERSON_NAME = "Mayank Kumar";
+export const PERSON_ROLE = "Frontend Engineer";
 
 export const HERO_CONTENT = `I am a passionate full stack developer with a knack for crafting robust and scalable web applications. With 5 years of hands-on experience, I have honed my skills in front-end technologies like React and Next.js, as well as back-end technologies like Node.js, MySQL, PostgreSQL, and MongoDB. My goal is to leverage my expertise to create innovative solutions that drive business growth and deliver exceptional user experiences.`;
 
